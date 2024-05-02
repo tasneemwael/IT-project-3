43c0d3aaad8659a6c5947c9fa3db2df988f509c8
tasneemwael <93452467+tasneemwael@users.noreply.github.com>
2024-05-02 21:56:25 +0300

    Update cart.js

--- Add to cart/scripts/cart.js
+++ Add to cart/scripts/cart.js
@@ -1,3 +1,21 @@
+function searchProducts() {
+var searchTerm = document.getElementById('searchInput').value.trim().toLowerCase();
+var products = document.querySelectorAll('.product');
+
+products.forEach(function(product) {
+  var productName = product.querySelector('.product-info h2').textContent.toLowerCase();
+  if (productName.includes(searchTerm)) {
+    product.style.display = 'block'; 
+  } else {
+    product.style.display = 'none'; 
+  }
+});
+}
+
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+event.preventDefault(); 
+searchProducts();
+});
 function themeChange() {
     const themeButton = document.getElementById('themeSelect');
     const themeStyleSheet = document.getElementById('themeStylesheet');
